8d147720ad5b502b05a2a77a7a533e3a25faf873
Mick Staugaard <mick@staugaard.com>
2012-10-27 11:37:12 -0700

    allow specifying the path of captured images

--- lib/rupi/camera.rb
+++ lib/rupi/camera.rb
@@ -17,10 +17,19 @@ module Rupi
 
     def self.capture(options = {})
       cfg = CONFIGURATION.merge(options)
-      file = Tempfile.new('snap')
+
+      path = options[:path]
+
+      if path.nil?
+        file = Tempfile.new('snap')
+        path = file.path
+      end
+
       command = "uvccapture -S#{cfg[:saturation]} -B#{cfg[:brightness]} -C#{cfg[:contrast]} -G#{cfg[:gain]} -x#{cfg[:width]} -y#{cfg[:height]}"
-      system("#{command} -o#{file.path}")
-      file
+
+      system("#{command} -o#{path}")
+
+      file || File.new(path)
     end
   end
 end
